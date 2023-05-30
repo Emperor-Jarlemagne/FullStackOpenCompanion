a47c39b52fb3116bf7a060ac184a97fd070df510
Emperor-Jarlemagne <104075576+Emperor-Jarlemagne@users.noreply.github.com>
2023-05-30 12:44:21 +0200

    Initial Commit

---
+++ ReduxProject/ReduxReactQuery/query-notes/src/App.js
@@ -0,0 +1,53 @@
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { getNotes, createNote, updateNote } from '../requests/requests'
+
+const App = () => {
+const queryClient = useQueryClient()
+
+  const newNoteMutation = useMutation(createNote, {
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData('notes')
+      queryClient.setQueryData('notes', notes.concat(newNote))
+    }
+  })
+
+  const addNote = async (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    newNoteMutation.mutate({ content, important: true })
+  }
+
+  const toggleImportance = (note) => {
+    console.log('toggle importance of', note.id)
+  }
+
+  const result = useQuery('notes', getNotes, {
+    refetchOnWindowFocus: False
+  })
+
+
+  if ( result.isLoading ) {
+    return <div>Loading data...</div>
+  }
+
+  const notes = result.data
+
+  return(
+    <div>
+      <h2>Notes app</h2>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      {notes.map(note =>
+        <li key={note.id} onClick={() => toggleImportance(note)}>
+          {note.content} 
+          <strong> {note.important ? 'important' : ''}</strong>
+        </li>
+      )}
+    </div>
+  )
+}
+
+export default App
